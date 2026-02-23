8e663f8429d477560c4d1bed6257ee9d5b9a5a3d
DP870 <projectdhruv0@gmail.com>
2026-02-23 12:39:18 +0530

    Guessing Game application intialization

---
+++ guessingGame.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+class GameConfig{
+	private final int MIN=1;
+	private final int MAX=100;
+	private final int MAX_ATTEMPTS=7;
+	private final int MAX_HINTS=3;
+	
+	
+	int targetNumber;
+	
+	public GameConfig() {
+		
+		Random random=new Random();
+		this.targetNumber=random.nextInt(MAX-MIN+1)+MIN;
+		
+		
+	}
+	
+	public int getTargetNumber(){ return targetNumber;}
+	
+	public int getMaxAttempts() { return MAX_ATTEMPTS;}
+	
+	public int getMaxHints() {return MAX_HINTS;}
+	
+	public void showRules(){
+		System.out.println("Guess a number between "+MIN+" and "+MAX);
+		System.out.println("You have "+ MAX_ATTEMPTS+" attempts.");
+		System.out.println("Hints will be provieded after wrong guesses.");
+		
+	}
+}
+
+/**
+* GuessingApp -Use Case 1: Game Intialization
+* This class serves as the application entry point.
+* It initializes the game configuration and displays game rules.
+* No user input or gameplay logic is implemented at this stage.
+* @author Dhruv
+* @version 1.0
+*/
+
+public class guessingGame{
+	public static void main(String[] args){
+		System.out.println("Welcome to the Guessing App");
+		GameConfig gameConfig = new GameConfig();
+		gameConfig.showRules();
+		
+		
+	}
+	
+	
+	
+}
